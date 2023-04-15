d96e2bb31b12568739c0d140de2260cd37350b3b
benedetta16 <benedettamazzetti16@gmail.com>
2023-04-15 20:06:51 +0200

    stanza 9 ok

--- Assets/Scripts/New/PorteTrigger_Quiz9.cs
+++ Assets/Scripts/New/PorteTrigger_Quiz9.cs
@@ -9,12 +9,17 @@ public class PorteTrigger_Quiz9 : MonoBehaviour
     private Vector3 initialLocalPosition;
     public GameObject Canvas_Quiz9_1; //Congratulazioni_Canvas
     public GameObject Canvas_Quiz9_2;
+    public GameObject Canvas_Quiz9_3;
+    public GameObject Canvas_Quiz9_4;
 
     // Start is called before the first frame update
     void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
         Canvas_Quiz9_1.SetActive(false);
         Canvas_Quiz9_2.SetActive(false);
+        Canvas_Quiz9_3.SetActive(false);
+        Canvas_Quiz9_4.SetActive(false);
         initialLocalPosition = GameObject.FindWithTag("Player").transform.localPosition;
         
     }
@@ -31,17 +36,42 @@ public class PorteTrigger_Quiz9 : MonoBehaviour
 
     IEnumerator Piano9()
     {
+        Cursor.lockState = CursorLockMode.None; 
+
         Canvas_Quiz9_1.SetActive(true);
 
-        yield return new WaitForSecondsRealtime(3);
+        yield return new WaitForSecondsRealtime(2);
 
         Canvas_Quiz9_1.SetActive(false);
 
-        yield return new WaitForSecondsRealtime(3);
+        yield return new WaitForSecondsRealtime(2);
 
         Canvas_Quiz9_2.SetActive(true);
 
-        yield return new WaitForSecondsRealtime(3);
+        yield return new WaitForSecondsRealtime(2);
+
+        Canvas_Quiz9_2.SetActive(false);
+
+        yield return new WaitForSecondsRealtime(2);
+
+        Canvas_Quiz9_3.SetActive(true);
+
+        yield return new WaitForSecondsRealtime(2);
+
+        Cursor.lockState = CursorLockMode.None; 
+
+        Canvas_Quiz9_3.SetActive(false);
+
+        yield return new WaitForSecondsRealtime(2);
+
+
+
+        //  Cursor.lockState = CursorLockMode.Locked;
+
+        Canvas_Quiz9_4.SetActive(true);
+
+
+
 
 
 
